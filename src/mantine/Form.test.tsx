a3d1c9fbd80bd733cb127149f8131c0d90e3c794
hiterm <hiterm23@gmail.com>
2022-10-17 22:11:53 +0900

    Write test for mantine form

--- src/mantine/Form.test.tsx
+++ src/mantine/Form.test.tsx
@@ -1,9 +1,19 @@
-import { render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { Form } from './Form';
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
+import { Form, FormValue } from "./Form";
 
-test('type', async () => {
-  const { getByLabelText } = render(<Form />);
+test("type", async () => {
+  const handleSubmit = vi.fn((values: FormValue) => {});
+  const { getByLabelText, getByRole } = render(
+    <Form onSubmit={handleSubmit} />
+  );
   const user = userEvent.setup();
-  await user.type(getByLabelText('Email'), 'a@example.com');
+  await user.type(getByRole("textbox", { name: "Email" }), "a@example.com");
+  await user.click(getByRole("button"));
+  expect(handleSubmit.mock.calls.length).toBe(1);
+  expect(handleSubmit.mock.calls[0][0]).toStrictEqual({
+    email: "a@example.com",
+    termsOfService: false,
+  });
 });
--- src/mantine/Form.tsx
+++ src/mantine/Form.tsx
@@ -1,32 +1,35 @@
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { useForm } from "@mantine/form";
 
-export const Form: React.FC = () => {
+export type FormValue = { email: string; termsOfService: boolean };
+type FormProps = { onSubmit: (values: FormValue) => void };
+
+export const Form: React.FC<FormProps> = ({ onSubmit }) => {
   const form = useForm({
     initialValues: {
-      email: '',
+      email: "",
       termsOfService: false,
     },
 
     validate: {
-      email: (value) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
     },
   });
 
   return (
     <Box sx={{ maxWidth: 300 }} mx="auto">
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
+      <form onSubmit={form.onSubmit((values) => onSubmit(values))}>
         <TextInput
           withAsterisk
           label="Email"
           placeholder="your@email.com"
-          {...form.getInputProps('email')}
+          {...form.getInputProps("email")}
         />
 
         <Checkbox
           mt="md"
           label="I agree to sell my privacy"
-          {...form.getInputProps('termsOfService', { type: 'checkbox' })}
+          {...form.getInputProps("termsOfService", { type: "checkbox" })}
         />
 
         <Group position="right" mt="md">
